7daeeecba1a944a8c85fdeec4814dad07d3c6a78
DorelPislan <pislan@gmail.com>
2023-06-07 21:40:25 +0300

    Added a non-moveable class

--- MoveSemantics.cpp
+++ MoveSemantics.cpp
@@ -1,20 +1,83 @@
-// MoveSemantics.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
+#include <format>
+
+class NonMoveable
+{
+public:
+    //class default c-tor
+    NonMoveable()
+    {
+        mInstanceId = kInstanceId++;
+
+        Trace("Default ctor");
+    }
+
+    //class c-tor
+    NonMoveable(const char* aRes)
+    {
+        mInstanceId = kInstanceId++;
+        SetResource(aRes);
+        Trace("Non-Default ctor");
+    }
+
+    //copy c-tor
+    NonMoveable(const NonMoveable& aOther)
+        : NonMoveable(aOther.mResource)
+    {
+        mInstanceId = kInstanceId++;
+        Trace("Copy ctor");
+    }
+
+    //class d-tor
+    ~NonMoveable()
+    {
+        Trace("Destructor");
+
+        delete mResource;
+    }
+
+    void SetResource(const char* aNewData)
+    {
+        size_t newResLength = strlen(aNewData) + 1;
+        mResource = new char[newResLength];
+        strcpy_s(mResource, newResLength, aNewData);
+
+        Trace("SetResource()");
+    }
+
+    NonMoveable& operator=(const NonMoveable & aOther)
+    {
+        size_t newResLength = strlen(aOther.mResource) + 1;
+
+        char* newData = new char[newResLength];
+
+        strcpy_s(newData, newResLength, aOther.mResource);
+
+        mResource = newData;
+        Trace("operator = ");
+
+        return *this;        
+    }
+private:
+    inline static  int kInstanceId = 1;
+
+    char* mResource{};
+    int mInstanceId = 0;
+
+    void Trace(const char * aLabel)
+    {
+        std::cout << std::format("{:25} from instance: {}. Current resource is: \"{}\"\n",
+            aLabel, mInstanceId, ( mResource ? mResource : ""));
+    }
+};
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Hello Move semantics!\n";
+
+    NonMoveable def;
+    NonMoveable some("some text");
+    NonMoveable copied(some);
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
